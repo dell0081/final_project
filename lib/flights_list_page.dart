c7daab988b734cbe6d3a4faf9091e12132d2ced0
Khushpreet Kaur <khushisainikhushi148@gmail.com>
2024-07-28 14:18:08 -0400

    Fixed gitignore

---
+++ lib/flights_list_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightsListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flights List'),
+      ),
+      body: Center(
+        child: Text('This is the Flights List Page'),
+      ),
+    );
+  }
+}
